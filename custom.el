3f8a08d09b018f6f9e7fe26f3d5ff12bcda851c8
vicmaeg <vmartos@outlook.com>
2026-03-17 19:19:05 +0100

    feat: initial config based on prot basic config

---
+++ custom.el
@@ -0,0 +1,23 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e9835d67c171416877752f0aa0293b1f2070e5e011fb76419bae0adead7cbd45"
+     "21c4c4b7d3ab161aaa28b15ca846854d395c33cfb7c6863ab601adfe10d70ce0"
+     "1b7e575c6681e66d8d83634c2c160b40af12f3756360a4dd81b8032f4495cb5e"
+     "c181188a9793b37d054e48e919fbdcfd1dc658b675a349b1399fdc2c8edaeec7"
+     "a75fc55d480accd6f651d1bae492b7ab4c5b28894350b63ef39bf947bb8fd453"
+     "8fbf2d585f1138caaafa9e523fa3a20614c1d1dcc6002c9808c3e40028e21df4"
+     default))
+ '(package-selected-packages
+   '(corfu dired-subtree marginalia modus-themes nerd-icons-completion
+	   nerd-icons-corfu nerd-icons-dired orderless trashed vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
