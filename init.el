58677d49de7fffd30296b42023ee59b31e78351b
vicmaeg <vmartos@outlook.com>
2026-04-14 22:33:01 +0200

    feat: add diagnostics support and yasnippet
    diagnostic support has been copied from https://github.com/emacs-lsp/lsp-mode/issues/4740

--- init.el
+++ init.el
@@ -236,7 +236,21 @@ The DWIM behaviour of this command is as follows:
       (minibuffer-with-setup-hook
           (lambda ()
             (insert completion))
-        (call-interactively #'embark-act)))))
+         (call-interactively #'embark-act)))))
+
+(use-package yasnippet
+  :ensure t
+  :hook (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map [tab] nil)
+  (define-key yas-minor-mode-map (kbd "C-c s") #'yas-expand))
+
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet
+  :config (yas-reload-all))
 
 ;;; Kill ring and clipboard improvements
 
@@ -400,14 +414,125 @@ The DWIM behaviour of this command is as follows:
   (setq lsp-enable-symbol-highlighting nil)
   (setq lsp-enable-on-type-formatting nil)
   (setq lsp-enable-code-lens nil)
+  (setq lsp-enable-snippet t)
   (setq lsp-signature-auto-activate nil)
   (setq lsp-modeline-code-actions-enable nil)
   (setq lsp-modeline-diagnostics-enable nil)
   (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-diagnostics-provider :flycheck)
+  (setq lsp-diagnostics-provider :flymake)
   (setq lsp-roslyn-server-log-level "Warning")
-  (setq gc-cons-threshold 100000000)
+  (gc-cons-threshold 100000000)
   (lsp-enable-which-key-integration))
+
+(require 'lsp-mode)
+
+(defun custom-capabilities-override (&optional custom-capabilities)
+  "Return the client capabilities appending CUSTOM-CAPABILITIES."
+  (append
+   `((general . ((positionEncodings . ["utf-32", "utf-16"])))
+     (workspace . ((workspaceEdit . ((documentChanges . t)
+                                     (resourceOperations . ["create" "rename" "delete"])))
+                   (applyEdit . t)
+                   (symbol . ((symbolKind . ((valueSet . ,(apply 'vector (number-sequence 1 26)))))))
+                   (executeCommand . ((dynamicRegistration . :json-false)))
+                   ,@(when lsp-enable-file-watchers '((didChangeWatchedFiles . ((dynamicRegistration . t)))))
+                   (workspaceFolders . t)
+                   (configuration . t)
+                   ,@(when lsp-semantic-tokens-enable
+                       `((semanticTokens . ((refreshSupport . ,(or (and (boundp 'lsp-semantic-tokens-honor-refresh-requests)
+                                                                        lsp-semantic-tokens-honor-refresh-requests)
+                                                                   :json-false))))))
+                   ,@(when lsp-lens-enable '((codeLens . ((refreshSupport . t)))))
+                   ,@(when lsp-inlay-hint-enable '((inlayHint . ((refreshSupport . :json-false)))))
+                   (diagnostics . ((refreshSupport . :json-false)))
+                   (fileOperations . ((didCreate . :json-false)
+                                      (willCreate . :json-false)
+                                      (didRename . t)
+                                      (willRename . t)
+                                      (didDelete . :json-false)
+                                      (willDelete . :json-false)))))
+     (textDocument . ((declaration . ((dynamicRegistration . t)
+                                      (linkSupport . t)))
+                      (definition . ((dynamicRegistration . t)
+                                     (linkSupport . t)))
+                      (references . ((dynamicRegistration . t)))
+                      (implementation . ((dynamicRegistration . t)
+                                         (linkSupport . t)))
+                      (typeDefinition . ((dynamicRegistration . t)
+                                         (linkSupport . t)))
+                      (synchronization . ((willSave . t) (didSave . t) (willSaveWaitUntil . t)))
+                      (documentSymbol . ((symbolKind . ((valueSet . ,(apply 'vector (number-sequence 1 26)))))
+                                         (hierarchicalDocumentSymbolSupport . t)))
+                      (formatting . ((dynamicRegistration . t)))
+                      (rangeFormatting . ((dynamicRegistration . t)))
+                      (onTypeFormatting . ((dynamicRegistration . t)))
+                      ,@(when (and lsp-semantic-tokens-enable
+                                   (functionp 'lsp--semantic-tokens-capabilities))
+                          (lsp--semantic-tokens-capabilities))
+                      (rename . ((dynamicRegistration . t) (prepareSupport . t)))
+                      (codeAction . ((dynamicRegistration . t)
+                                     (isPreferredSupport . t)
+                                     (codeActionLiteralSupport . ((codeActionKind . ((valueSet . ["" "quickfix" "refactor" "refactor.extract" "refactor.inline" "refactor.rewrite" "source" "source.organizeImports"])))))
+                                     (resolveSupport . ((properties . ["edit" "command"])))
+                                     (dataSupport . t)))
+                      (completion . ((completionItem . ((snippetSupport . ,(cond
+                                                                            ((and lsp-enable-snippet (not (fboundp 'yas-minor-mode)))
+                                                                             (lsp--warn (concat "Yasnippet is not installed, but `lsp-enable-snippet' is set to `t'. "
+                                                                                                "You must either install yasnippet, or disable snippet support."))
+                                                                             :json-false)
+                                                                            (lsp-enable-snippet t)
+                                                                            (t :json-false)))
+                                                        (documentationFormat . ["markdown" "plaintext"])
+                                                        (resolveAdditionalTextEditsSupport . t)
+                                                        (insertReplaceSupport . t)
+                                                        (deprecatedSupport . t)
+                                                        (resolveSupport
+                                                         . ((properties . ["documentation" "detail" "additionalTextEdits" "command"])))
+                                                        (insertTextModeSupport . ((valueSet . [1 2])))
+                                                        (labelDetailsSupport . t)))
+                                     (contextSupport . t)
+                                     (dynamicRegistration . t)))
+                      (signatureHelp . ((signatureInformation . ((parameterInformation . ((labelOffsetSupport . t)))
+                                                                 (activeParameterSupport . t)))
+                                        (dynamicRegistration . t)))
+                      (documentLink . ((dynamicRegistration . t)
+                                       (tooltipSupport . t)))
+                      (hover . ((contentFormat . ["markdown" "plaintext"])
+                                (dynamicRegistration . t)))
+                      ,@(when lsp-enable-folding
+                          `((foldingRange . ((dynamicRegistration . t)
+                                             ,@(when lsp-folding-range-limit
+                                                 `((rangeLimit . ,lsp-folding-range-limit)))
+                                             ,@(when lsp-folding-line-folding-only
+                                                 `((lineFoldingOnly . t)))))))
+                      (selectionRange . ((dynamicRegistration . t)))
+                      (callHierarchy . ((dynamicRegistration . :json-false)))
+                      (typeHierarchy . ((dynamicRegistration . t)))
+                      (publishDiagnostics . ((relatedInformation . t)
+                                             (tagSupport . ((valueSet . [1 2])))
+                                             (versionSupport . t)))
+                      (diagnostic . ((dynamicRegistration . t)
+                                     (relatedDocumentSupport . t)))
+                      (linkedEditingRange . ((dynamicRegistration . t)))
+                      (inlineCompletion . ())
+                      ,@(when lsp-inlay-hint-enable '((inlayHint . ((dynamicRegistration . :json-false))))))))
+   custom-capabilities))
+
+(advice-add 'lsp--client-capabilities :override #'custom-capabilities-override)
+
+(defun custom-lsp-roslyn--on-initialized (workspace)
+  (lsp-roslyn-open-solution-file)
+  (with-lsp-workspace workspace
+    (lsp--set-configuration
+     #s(hash-table
+        size 30
+        test equal
+        data (
+              "csharp|background_analysis.dotnet_analyzer_diagnostics_scope" "fullSolution"
+              "csharp|background_analysis.dotnet_compiler_diagnostics_scope" "fullSolution"
+              )))))
+
+(advice-add 'lsp-roslyn--on-initialized :override #'custom-lsp-roslyn--on-initialized)
 
 (use-package which-key
   :ensure nil
@@ -438,9 +563,9 @@ The DWIM behaviour of this command is as follows:
 ;; Enable electric-pair-mode for better brace handling in C#
 (add-hook 'csharp-ts-mode-hook #'electric-pair-local-mode)
 
-(use-package flycheck
-  :ensure t
-  :hook (csharp-ts-mode . flycheck-mode))
+(use-package flymake
+  :ensure nil
+  :hook (csharp-ts-mode . flymake-mode))
 
 ;;; Dotnet CLI integration
 
