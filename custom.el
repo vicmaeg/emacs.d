812134c64727c128e8d8d2256a71d6afabffa8b4
vicmaeg <vmartos@outlook.com>
2026-07-25 21:58:29 +0200

    feat: change some keybindings to be more terminal friendly

--- custom.el
+++ custom.el
@@ -34,11 +34,10 @@
    '(avy cape consult-denote corfu denote-journal denote-markdown diff-hl
          dired-subtree doric-themes ef-themes embark-consult
          expand-region fontaine ghostel git-link gruber-darker-theme
-         lsp-mode magit marginalia move-text multiple-cursors
-          nerd-icons-completion nerd-icons-corfu nerd-icons-dired orderless
-          perspective
-          sharper trashed treesit-auto vertico
-          yasnippet-snippets)))
+         kanagawa-themes lsp-mode magit marginalia move-text
+         multiple-cursors nerd-icons-completion nerd-icons-corfu
+         nerd-icons-dired orderless perspective sharper trashed
+         treesit-auto vertico yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
